d1bbf52603d2278117c7b0bcc606c8f34e005371
SarahAvery <sarahashavery@gmail.com>
2020-10-21 18:38:28 -0400

    services cards made - No background img yet.

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\img\\banner-background.jpg":[["banner-background.7df10ede.jpg","resources/img/banner-background.jpg"],"resources/img/banner-background.jpg"],"_css_loader":"../../../../AppData/Local/Yarn/Data/global/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"js/index.js":[function(require,module,exports) {
+},{"./..\\img\\banner-background.jpg":[["banner-background.7df10ede.jpg","resources/img/banner-background.jpg"],"resources/img/banner-background.jpg"],"./..\\img\\services-cropped.jpg":[["services-cropped.1f77568d.jpg","resources/img/services-cropped.jpg"],"resources/img/services-cropped.jpg"],"_css_loader":"../../../../AppData/Local/Yarn/Data/global/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"js/index.js":[function(require,module,exports) {
 "use strict";
 
 require("../resources/sass/main");
@@ -223,7 +223,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62274" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50572" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
